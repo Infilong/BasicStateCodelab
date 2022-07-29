5d00174feb60d83194720e65ef66a8b9483aa54e
infilong <ytxqfe@gmail.com>
2022-07-29 21:47:44 +0800

    Removing a task from list with observable mutableList

--- app/src/main/java/com/example/basicstatecodelab/WellnessTaskItem.kt
+++ app/src/main/java/com/example/basicstatecodelab/WellnessTaskItem.kt
@@ -40,12 +40,12 @@ fun WellnessTaskItem(
 }
 
 @Composable
-fun WellnessTaskItem(taskName: String, modifier: Modifier = Modifier) {
+fun WellnessTaskItem(taskName: String, onClose: () -> Unit, modifier: Modifier = Modifier) {
     var checkedState by rememberSaveable { mutableStateOf(false) }
 
     WellnessTaskItem(
         taskName = taskName,
-        onClose = { },
+        onClose = onClose,
         checked = checkedState,
         onCheckedChange = { newValue -> checkedState = newValue },
         modifier = modifier
